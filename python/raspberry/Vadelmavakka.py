01c35b8a1f5209c513cda219b1c3a2bcfe5db78e
kit548 <kit548@gmail.com>
2019-01-11 15:36:31 +0200

    tallennusraja-arvo lisätty

--- python/raspberry/Vadelmavakka.py
+++ python/raspberry/Vadelmavakka.py
@@ -7,32 +7,41 @@ class Mittausluokka:
         self.kaasunimi = kaasunimi
         self.gageid = gageid
         self.kaasuid = kaasuid 
-        self.muutos_raja_arvo = muutos_raja_arvo
-        self.pakotettu_tallennusvali = pakotettu_tallennusvali
-        self.talletettuarvo = 0
-        self.talletus_aika = 0
+        self.muutos_raja_arvo = float(muutos_raja_arvo)
+        self.pakotettu_tallennusvali = float(pakotettu_tallennusvali)
+        self.tallennuksen_alaraja = -sys.float_info.max
+        self.talletettuarvo = 0.0
+        self.talletus_aika = 0.0
         self.edellinen_tallentamatta = False
         #mongo db asetuksia
         myclient = pymongo.MongoClient("mongodb://localhost:27017/")
         mydb = myclient["IoTGas"]
         self.mycol = mydb["gasgage"]
+        
+    def tallennuksen_alaraja(self,raja):
+        self.tallennuksen_alaraja = float(raja)
 
     def talletakantaan(self, mitattu_arvo, mittausaika):
-        self.edellinen_tallentamatta = False 
-        self.talletettuarvo = mitattu_arvo
-        self.talletus_aika = mittausaika
-        mittajson = {"gageid": self.gageid , "kaasuid": self.kaasuid , "kaasunimi": self.kaasunimi, "arvo": mitattu_arvo, "gagetime": mittausaika}
-        x = self.mycol.insert(mittajson)
+        if mitattu_arvo > self.tallennuksen_alaraja:
+            self.edellinen_tallentamatta = False 
+            self.talletettuarvo = mitattu_arvo
+            self.talletus_aika = mittausaika
+            mittajson = {"gageid": self.gageid , "kaasuid": self.kaasuid , "kaasunimi": self.kaasunimi, "arvo": mitattu_arvo, "gagetime": mittausaika}
+            x = self.mycol.insert(mittajson)
+        else:
+            print("Arvo " + str(mitattu_arvo) + " alle alarajan " + str(self.tallennuksen_alaraja))
 
     def edellinenmitta_jemmaan(self, mitattu_arvo, mittausaika):
-        self.edellinen_tallentamatta = True
-        self.edellinenmittajson = {"gageid": self.gageid , "kaasuid": self.kaasuid , "kaasunimi": self.kaasunimi, "arvo": mitattu_arvo, "gagetime": mittausaika }
+        if mitattu_arvo > self.tallennuksen_alaraja: 
+            self.edellinen_tallentamatta = True
+            self.edellinenmittajson = {"gageid": self.gageid , "kaasuid": self.kaasuid , "kaasunimi": self.kaasunimi, "arvo": mitattu_arvo, "gagetime": mittausaika }
         
     def talleta_edellinenmitta(self):
-        print("edellinen " + self.kaasunimi + " kantaan ")
-        # kaikenvaralta false :-) 
-        self.edellinen_tallentamatta = False
-        x = self.mycol.insert(self.edellinenmittajson)
+        if self.edellinenmittajson["arvo"] > self.tallennuksen_alaraja: 
+            print("edellinen " + self.kaasunimi + " kantaan " + str(self.edellinenmittajson["arvo"]))
+            # kaikenvaralta false :-) 
+            self.edellinen_tallentamatta = False
+            x = self.mycol.insert(self.edellinenmittajson)
             
     def huomattava_muutos_tullut_talleta_kantaan(self, mitattu_arvo, mittausaika):
         if abs(mitattu_arvo - self.talletettuarvo) > self.muutos_raja_arvo:
--- python/raspberry/vadelma_iotgas.py
+++ python/raspberry/vadelma_iotgas.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
-
 import time
 import os
 import math
@@ -30,81 +29,82 @@ def konsolitulostus(laskuri, mittausaika, mitattu_arvo, hajonta, lampo, hajonta_
     print("lampotila: %02f" % lampo + "   hajonta: %02f" % hajonta_temp)
     print(' ')
 
-# kalibraation tarkkuus noin 0,5 astetta lampotiloissa 20-25 astetta - analog temp sensor
+# kalibraation tarkkuus noin 0,5 astetta lampotiloissa 20-25 astetta - analog temp Rs sensor
 # https://www.aliexpress.com/item/37-Sensor-Module-Kit-For-Arduino-For-Raspberry-Pi-Model-B-With-GPIO-Extension-Jumper-for/32816585549.html?spm=a2g0s.9042311.0.0.76f24c4d4GQXz9
-def lampotila(vadc, hajonta = False):
+def analog_temp_Rs_sensor_kali(vadc, hajonta = False):
     if hajonta:
         return (28.72667 * vadc)
     else:
         return (28.72667 * vadc - 30.05396)
 
-def main():
-    
+def main():   
     #raspberry pi adc  
     adc = ADCPi(0x68, 0x69, 12)
 
+    print(sys.version)
     # mittausten aluastus, hieman kovakoodia 
     # pakotettu_tallennusvali minuuteissa 
-    pakotettu_tallennusvali = 10
+    pakotettu_tallennusvali = 5
     # tallennus raja volteissa anturilta 1 
     muutos_raja_arvo_1 = 0.03
     # mittauksen eli kaasun nimi ja muuta kovakoodausta  
-    kaasunimi_1 = "Ilma (kynnys " + str(muutos_raja_arvo_1) +")"
+    kaasunimi_1 = "Leipomo (kynnys " + str(muutos_raja_arvo_1) +")"
     gageid = "01"
     kaasuid = "01"    
-    # ja tietoja mittausluokalle ja samalla luokka -> mittaus 
+    # ja sitten alustetaan anturi 1 ilmat etc. ja anturi 2 lampotila
     mittaus = Vadelmavakka.Mittausluokka(kaasunimi_1, gageid, kaasuid, muutos_raja_arvo_1, pakotettu_tallennusvali)
-    lampomittaus = Vadelmavakka.Mittausluokka("Lampotila", "02", "90")
-
+    analog_temp_Rs_sensor = Vadelmavakka.Mittausluokka("Lampotila", "02", "90")
+    analog_temp_Rs_sensor.tallennuksen_alaraja = -30 
+    
     #sleep_mittausvali_keskiarvoon sekunneissa eli pieni tauko silmukkaan -> time.sleep(secs)
     sleep_mittausvali_keskiarvoon = 0.05
     
-    # laskuri mitttauksille... (just 4fun)
+    # laskuri mitttauksille... (just4fun)
     laskuri = 0  
     print(' ')
     print("Mittaus: " + mittaus.kaasunimi)
     print(' ')
 
     while True:
-        i = 0
-	silmukka_lkm = 10
-	mitat = []
-	lammot = []
-	while i < silmukka_lkm:
+        i = 0 
+        silmukka_lkm=10
+        mitat = [] 
+        lammot = [] 
+        while i < silmukka_lkm: 
             mitattu_arvo_1 = adc.read_voltage(1) 
-	    adc_temp = adc.read_voltage(2)
+            adc_temp = adc.read_voltage(2)
             #print("Channel 1: %02f" % adc.read_voltage(1))
 	    #...
 	    #print("Channel 8: %02f" % adc.read_voltage(8))
-	    mitat.append(mitattu_arvo_1)
-	    lammot.append(adc_temp) 
-	    time.sleep(sleep_mittausvali_keskiarvoon)
-	    i += 1
+            mitat.append(mitattu_arvo_1) 
+            lammot.append(adc_temp) 
+            time.sleep(sleep_mittausvali_keskiarvoon) 
+            i += 1
 	# mongodb aika millisek. 
         mittausaika_1 = int(round((time.time() - sleep_mittausvali_keskiarvoon*silmukka_lkm/2)* 1000))
-	mitattu_arvo_1 = statistics.mean(mitat)
+        mitattu_arvo_1 = statistics.mean(mitat) 
+        hajonta_1 = statistics.pstdev(mitat) 
 	# mitattu_arvo_1 kalibraatio funktio...?
-	hajonta_1 = statistics.pstdev(mitat)
+
+        adc_temp = statistics.mean(lammot) 
+        lampo = analog_temp_Rs_sensor_kali(adc_temp)
+	#lampo = 40 # testi 
+	hajonta_temp = statistics.pstdev(lammot) 
+	hajonta_temp = analog_temp_Rs_sensor_kali(hajonta_temp, True)
 	
-	adc_temp = statistics.mean(lammot)
-	lampo = lampotila(adc_temp)
-	hajonta_temp = statistics.pstdev(lammot)
-	hajonta_temp = lampotila(hajonta_temp, True)
-	
-	# login seurantaa varten laskuri 
-        laskuri += 1 
+	laskuri += 1 
 
         if mittaus.huomattava_muutos_tullut_talleta_kantaan(mitattu_arvo_1, mittausaika_1):
-            if lampomittaus.edellinen_tallentamatta:
-                lampomittaus.talleta_edellinenmitta()
-            lampomittaus.talletakantaan(lampo, mittausaika_1)
+            if analog_temp_Rs_sensor.edellinen_tallentamatta:
+                analog_temp_Rs_sensor.talleta_edellinenmitta()
+            analog_temp_Rs_sensor.talletakantaan(lampo, mittausaika_1)
             konsolitulostus(laskuri, mittausaika_1, mitattu_arvo_1, hajonta_1, lampo, hajonta_temp)
         elif mittaus.riittavan_kauvan_mennyt_talleta_kantaan(mittausaika_1, mitattu_arvo_1):
-            lampomittaus.talletakantaan(lampo, mittausaika_1)
+            analog_temp_Rs_sensor.talletakantaan(lampo, mittausaika_1)
             konsolitulostus(laskuri, mittausaika_1, mitattu_arvo_1, hajonta_1, lampo, hajonta_temp, pakotettu_tallennusvali)
         else:
             mittaus.edellinenmitta_jemmaan(mitattu_arvo_1, mittausaika_1)
-            lampomittaus.edellinenmitta_jemmaan(lampo, mittausaika_1)
+            analog_temp_Rs_sensor.edellinenmitta_jemmaan(lampo, mittausaika_1)
             
         # wait x seconds before reading the pins again
         time.sleep(1.0)
