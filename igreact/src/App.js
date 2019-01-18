e70c8ce8ca570cebadfb961bce2dd0df3c672880
kit548 <kit548@gmail.com>
2019-01-18 13:05:46 +0200

    otsikko (bootstrap test)

--- igreact/src/App.js
+++ igreact/src/App.js
@@ -5,7 +5,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-		  <h2>IoTGas</h2>
+		  <strong> IoTGas  </strong>
   		<MesoContainer/>
       </div>
     );
