d8e3457fc4f88e6edf9b5576fab3385194e9dd46
Lassi Eloranta <lassi.eloranta@gmail.com>
2019-02-06 12:33:36 +0200

    Erotettu REST-rajapinta koodi kokeeksi omaksi tiedostoksi ja kommentoitu vähän.

--- python/raspberry/dhthumidity.py
+++ python/raspberry/dhthumidity.py
@@ -3,39 +3,25 @@ DHT-kirjaston asennus
 Varmista että tarvittavat paketit löytyy, build-essential python-dev
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 sudo python setup.py install
+
+DHT11-pinout edestä
+Signal, VCC, Ground
 '''
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import sys
+import requests
+import time
 import Adafruit_DHT
+import restpinta
 
-gpioPin = 10
+# Sensorin datapinni Raspissa & mallin määritys
+gpioPin = 17
 dhtModel = Adafruit_DHT.DHT11
 
+# Yrittää lukea DHT-anturin arvot 15 kertaa kahden sekunnin välein
 humidity, temperature = Adafruit_DHT.read_retry(dhtModel, gpioPin)
 
-#Debug-tulostus
+# Debug-tulostus
 print('Ilmankosteus: {0:0.1f}%'.format(humidity))
 
-# REST      
-def _url(path):
-    return 'http://localhost:3010' + path
-
-def get_mesos():
-    return requests.get(_url('/meso/gasnames'))
-
-def add_meso(mittajson):
-    return requests.post(_url('/meso/create'), mittajson)
-# REST  
-    
-def main():
-    print()
-    paluu = get_mesos().content
-    print("Mittaukset: " + str(paluu))
-    paluu = ''
-    print("REST: " , _url('/meso/gasnames'))
-    print()
-    mittaus = 0
-    gageid = "dht11" 
-    kaasuid = "humidity"
-    kaasunimi = "DHT11"
---
+++ python/raspberry/restpinta.py
@@ -0,0 +1,8 @@
+ def _url(path):
+    return 'http://localhost:3010' + path
+
+def get_mesos():
+    return requests.get(_url('/meso/gasnames'))
+
+def add_meso(mittajson):
+    return requests.post(_url('/meso/create'), mittajson)
